967f0237cc10b7a23c3f839bd63e4eea5ceeb7cd
Denis Castillón <orangeneko158@outlook.com>
2026-04-12 23:59:08 -0700

    fix: emacs ahora sí usa zsh

--- doomEmacs_old/.config/doom/config.el
+++ doomEmacs_old/.config/doom/config.el
@@ -1,11 +1,10 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
 ;; Usar zsh
-(setq explicit-shell-file-name "/bin/zsh")
+(setq shell-file-name "/bin/zsh")
 
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
-
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
