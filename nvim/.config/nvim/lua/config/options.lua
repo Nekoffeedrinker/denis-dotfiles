4809c90eb089543a5354ae81011b5c67255247ed
Denis Castillón <orangeneko158@outlook.com>
2026-04-15 18:30:12 -0700

    nvim: tabs -> espacios

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -3,7 +3,6 @@ vim.opt.relativenumber = true
 vim.opt.cursorline = true
 vim.opt.wrap = false
 vim.opt.expandtab = false
-vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.clipboard = "unnamedplus"
 vim.opt.swapfile = false
@@ -13,3 +12,24 @@ vim.diagnostic.config({
 })
 
 vim.cmd.colorscheme "catppuccin"
+
+-- Usar espacios en lugar de tabs
+vim.o.tabstop = 4       -- ancho del <Tab> 
+vim.o.softtabstop = 2   -- inderción/retroceso con Tab/Backspace
+vim.o.shiftwidth = 4    -- sangrías automáticas (>> <<, autoident)
+vim.o.expandtab = true  -- convierte tabs en espacios
+
+
+-- Tabs cortas para archivos nix
+local augroup = vim.api.nvim_create_augroup("NixTabs", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup,
+  pattern = "nix",
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.expandtab = true
+  end,
+})
