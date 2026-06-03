ac8d059bcf5282e16222d579650575aa97c300c4
Denis Castillón <orangeneko158@outlook.com>
2026-06-02 17:49:22 -0700

    nvim: activar el wordwrap

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,7 +1,8 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
-vim.opt.wrap = false
+vim.opt.wrap = true
+vim.opt.linebreak = true
 vim.opt.expandtab = false
 vim.opt.shiftwidth = 4
 vim.opt.clipboard = "unnamedplus"
