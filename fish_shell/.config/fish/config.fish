cf7753692e753434a948ea0d4ad4280dece9a60f
Denis Castillón <orangeneko158@outlook.com>
2026-06-29 12:05:39 -0700

    fish: prompt mediante starship

--- fish_shell/.config/fish/config.fish
+++ fish_shell/.config/fish/config.fish
@@ -1,4 +1,15 @@
 if status is-interactive
+
+    # quitar el mensaje de bienvenida
     set -g fish_greeting
+
+    # Promnt de Starship (con transcient mode)
+    starship init fish | source
+    enable_transience
+    function starship_transient_prompt_func
+        starship module character
+    end
+
+
     # Commands to run in interactive sessions can go here
 end
