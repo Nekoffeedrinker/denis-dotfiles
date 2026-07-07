5787890fdf89a50119c480fa473b14f7158bbc35
Denis Castillón <orangeneko158@outlook.com>
2026-07-07 12:45:05 -0700

    ls ya no muestra iconos en los pagers

--- fish_shell/.config/fish/conf.d/atajos-ls.fish
+++ fish_shell/.config/fish/conf.d/atajos-ls.fish
@@ -2,13 +2,13 @@
 #     Alias de eza para remplazar ls
 # ================================================
 
-alias eza='eza --color=always --icons=always --git'
+alias eza='eza --color=always --icons=auto --git'
 alias ezal='eza -l --no-permissions --no-filesize --no-user --no-time'
 
 abbr ls eza
 abbr lsa eza -a
 abbr ll eza -lg
-abbr lla eza -lga
+abbr lla eza -alg
 abbr l ezal
 abbr la ezal -a
 
